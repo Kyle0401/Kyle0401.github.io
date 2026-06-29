d83d989f12950ddb8020a44a92cf9e499750c0a9
Kyle <1154727104@qq.com>
2026-06-29 13:37:24 +0800

    style: remove section number badges from Java note

--- learning/java/java-language/assets/diagram-part-01.js
+++ learning/java/java-language/assets/diagram-part-01.js
@@ -1,2 +1,8 @@
 window.__javaDiagramParts = window.__javaDiagramParts || [];
 window.__javaDiagramParts.push("iVBORw0KGgoAAAANSUhEUgAABm0AAAEgCAMAAABhIschAAAA7VBMVEXd3d3U1NRi+v9h9fr/czdSyMxHo6dFnaFAq0dAqEY4iz8ydDm8XP/6cTfLYDKlUi+fUC44cXQycDgsUzByPyokLzIhKiY2KUQ1KUE0KUE3KEM2KEM2KEI1KEI0KEAzKD8zJz8yJz8yJz4xJz0xJjswJjwwJzovJzkvJjkuJjkuJjgtJTctJTYsJTYsJTUsJDQuJiUrJTUrJTMqJTMrJDUqJDMqJDIpJDEoJDAoIzAoIy8nIy8nIy4nIi0mIywlIywlIiwlIiskIiskIiojIikjISkiISgiISchISYhISUgISUiICYgICQfICMbFCIAXL2zAAAaoklEQVR42u3dC5vjxJWAYbmnB3oGz3DHJGRz2SSEy7IQQi4bYMIG2FwY6f//nH2YtmVJVSXJlmRL9vs9eSbdbbe7OV11Pp3SqXKWAwAwNZkQAADYBgDANgAAsA0AgG0AAGwDAADbAADYBgAAtgEAsA0AgG0AAGAbAADbAADYBgAAtgEAXIpt3nr65BZH8eTpWwbVGLz5+NEdjuLR4zeNnzF4/eGD");
+
+(function () {
+    var style = document.createElement('style');
+    style.textContent = '.article h2 .section-index { display: none !important; }';
+    document.head.appendChild(style);
+})();
